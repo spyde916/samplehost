7db29d74e62cb7b43d4c8e783f3a832ab57d5862
Manish <spyde916@gmail.com>
2019-11-16 21:37:46 +0530

    Updates

---
+++ precache-manifest.6df3ceb6b12a437b1356881d386ffecf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4157cac9fe5e0827c085c5195428b0af",
+    "url": "/samplehost/index.html"
+  },
+  {
+    "revision": "34178367855db75bbbc6",
+    "url": "/samplehost/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b74d313b5972c6f9edd1",
+    "url": "/samplehost/static/js/2.f94b6a06.chunk.js"
+  },
+  {
+    "revision": "34178367855db75bbbc6",
+    "url": "/samplehost/static/js/main.140b1a94.chunk.js"
+  },
+  {
+    "revision": "11093d7a6b1e32eb1d89",
+    "url": "/samplehost/static/js/runtime-main.b3e5d8c5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/samplehost/static/media/logo.25bf045c.svg"
+  }
+]);
